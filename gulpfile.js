ee5e70798fa5239df20490771ec94b0c2ba9d090
Marco Cameriero <me@marcocameriero.net>
2017-02-13 01:58:30 +0100

    Added `build` task to the Gulpfile to produce a bundled version of the app.

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,9 @@ var path = require('path'),
     gulp = require('gulp'),
     gutil = require('gulp-util'),
     eslint = require('gulp-eslint'),
+    minifyHtml = require('gulp-html-minifier'),
     webpack = require('webpack'),
+    webpackStream = require('webpack-stream'),
     WebpackDevServer = require('webpack-dev-server');
 
 var webpackConfig = require('./webpack.config');
@@ -41,4 +43,57 @@ gulp.task('serve', [], function () {
         gutil.log('[WebpackDevServer]', `Listening on port ${port}`);
     });
 
+});
+
+gulp.task('build', [ 'build-js', 'build-html', 'build-static' ]);
+
+gulp.task('build-js', function () {
+
+    // Release specific webpack config
+    webpackConfig.plugins.push(new webpack.DefinePlugin({
+        'process.env': {
+            NODE_ENV: JSON.stringify('production')
+        }
+    }));
+    webpackConfig.plugins.push(new webpack.optimize.UglifyJsPlugin());
+
+    return gulp.src('./src/index.js')
+        .pipe(webpackStream(webpackConfig))
+        .pipe(gulp.dest('./out'));
+});
+
+gulp.task('build-html', function () {
+    return gulp.src('./src/index.html')
+        .pipe(minifyHtml({
+            removeIgnored: false,
+            removeComments: true,
+            removeCommentsFromCDATA: true,
+            removeCDATASectionsFromCDATA: true,
+            collapseWhitespace: true,
+            conservativeCollapse: false,
+            collapseBooleanAttributes: true,
+            removeAttributeQuotes: true,
+            removeRedundantAttributes: true,
+            useShortDoctype: true,
+            removeEmptyAttributes: true,
+            removeEmptyElements: false,
+            removeOptionalTags: true,
+            removeScriptTypeAttributes: false,
+            removeStyleLinkTypeAttributes: false,
+            caseSensitive: false,
+            keepClosingSlash: false,
+            minifyJS: true,
+            processScripts: false,
+            minifyCSS: true,
+            minifyURLs: false,
+            lint: false
+        }))
+        .pipe(gulp.dest('./out'));
+});
+
+gulp.task('build-static', function () {
+    return gulp.src([
+        './src/assets/*.{json,mp3,png,jpg}'
+        ])
+        .pipe(gulp.dest('./out/assets'));
 });
